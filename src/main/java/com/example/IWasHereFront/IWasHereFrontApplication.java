b7aa117b52d48355449963e9cb0c7d7421a8bc03
pablo <pablonuserra@gmail.com>
2023-04-14 09:07:45 +0200

    First commit

---
+++ src/main/java/com/example/IWasHereFront/IWasHereFrontApplication.java
@@ -0,0 +1,13 @@
+package com.example.IWasHereFront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IWasHereFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IWasHereFrontApplication.class, args);
+	}
+
+}
